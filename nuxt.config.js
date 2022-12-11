be80880a637ba3066f586c90c2d2fe8ba1336dbd
Laddge <dev.laddge@gmail.com>
2022-12-11 00:16:11 +0000

    Font Awesome周りを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,6 +48,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      plugins: {
+        "postcss-custom-properties": false
+      }
+    }
   },
 
   content: {
