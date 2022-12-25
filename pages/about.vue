36e430b80e4e7401f785099b935edfae38ebd370
Laddge <dev.laddge@gmail.com>
2022-12-25 03:58:29 +0000

    aboutページを適当に作成(要編集)

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Header />
+    <div class="container text-center">
+      <a href="https://laddge.net/" target="_blank">
+        <button class="btn btn-primary-light btn-lg">
+          Portfolio
+        </button>
+      </a>
+    </div>
+    <Footer />
+  </div>
+</template>
